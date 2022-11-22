78f0cf5b80675ade4c90db5d4d6d8b7bb90f2774
ZhykharievAnatolii <nekijt87@gmail.com>
2022-11-22 21:14:05 +0200

    change files
    change js html files

--- src/index.js
+++ src/index.js
@@ -3,35 +3,7 @@
  *
  * @type {Date} date
  */
-const now=new Date();
-function updateTime(date) {
-    const hours=document.querySelector('.hours');
-    const minutes=document.querySelector('.minutes');
-    const seconds=document.querySelector('.seconds');
 
-    hours.innerText=date.getHours().toString().padStart(2, '0');
-    minutes.innerText=date.getMinutes().toString().padStart(2, '0');
-    seconds.innerText=date.getSeconds().toString().padStart(2, '0');
-
-}
-updateTime(now);
-// setInterval(()=>{
-// now.setSeconds(now.getSeconds()+1);
-//     updateTime(now);
-// },1000)
-const intervalId=setInterval(()=>{
-    now.setSeconds(now.getSeconds()+1);
-    updateTime(now);
-},1000);
-
-// function renderNotification(text) {
-//     const list=document.querySelector('ul')
-//     const notification=document.createElement('li');
-//     notification.innerText=text;
-//     list.append(notification);
-//     return notification;
-//
-// };
 function createCalendar(elem, year, month) {
     let mon = month - 1; // месяцы в JS идут от 0 до 11, а не от 1 до 12
     let d = new Date(year, mon);
@@ -70,31 +42,3 @@ function getDay(date) { // получить номер дня недели, от
 }
 
 createCalendar(calendar, 2022, 11);
-
-
-const button = document.querySelector('button');
-const list = document.querySelector('ul');
-const names = ['Bohdan', 'Stepan', 'Yura', 'Julia', 'Olena'];
-
-const renderItem = (text) => {
-    const item = document.createElement('li');
-    const button = document.createElement('button');
-    button.innerText = 'X';
-    item.innerText = text;
-    item.append(button);
-    button.onclick = () => {
-        item.remove();
-    };
-    item.onmousedown = () => {
-        item.classList.add('red');
-    };
-
-    item.onmouseup = () => {
-        item.classList.remove('red');
-    }
-
-
-    list.append(item);
-};
-
-names.forEach(renderItem);
